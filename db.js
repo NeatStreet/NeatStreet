5568cdd468226e1f4a11fe8789177538357c1938
loriluo95 <zluo2@wpi.edu>
2018-11-21 19:25:47 -0800

    add login

--- db.js
+++ db.js
@@ -1,32 +1,10 @@
 const pg = require('pg');
 
 const SQLconnection =
-  'postgres://xkvvjoiw:O6_92MrJi1A2YxAtGWFCO2PrqU3oJdGn@stampy.db.elephantsql.com:5432/xkvvjoiw';
+  'postgres://ehnepibw:IUAGevlvyuI3MQuwTZ8erUxYx1QwL8Vo@pellefant.db.elephantsql.com:5432/ehnepibw';
 const client = new pg.Client(SQLconnection);
 client.connect();
 
-allBooks();
-allBookReservations();
-
-function allBooks() {
-  client.query('SELECT * From books', (err, result) => {
-    // console.log('ISBN, Title, Authors');
-    // result.rows.forEach(row =>
-    //   console.log(`${row.isbn}, ${row.title}, ${row.authors}`)
-    // );
-  });
-}
-
-function allBookReservations(){
-  client.query('SELECT * From bookreserve', (err, result) => {
-    // console.log('ISBN, username, requesttime');
-    // result.rows.forEach(row =>
-    //   console.log(`${row.isbn}, ${row.username}, ${row.requesttime}`)
-    // );
-  });
-}
-
 module.exports = {
-  client,
-  allBooks
+  client
 };
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 
 const db = require('../db');
 
 router.get('/', (req, res) => {
-    const { username } = req.query
+    const { username } = req.query;
     res.json(req.query)
 })
 
 router.post('/', (req, res) => {
-    const { username, password } = req.body
+    const { email, password } = req.body;
     // console.log(req.body)
     // console.log(username+password);
     db.client.query(
-        `SELECT type FROM users WHERE username = '${username}' AND password = '${password}'`,
+        `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`,
         (err, result) => {
             // console.log(result.rows[0].type)
             res.json(result.rows)
