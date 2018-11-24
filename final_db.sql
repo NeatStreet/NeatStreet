3b3b98efb273b68326fbbe4264d869eb4ad5a93e
loriluo95 <zluo2@wpi.edu>
2018-11-24 14:09:57 -0800

    merge loriluo branch

--- final_db.sql
+++ final_db.sql
@@ -8,3 +8,15 @@ CREATE TABLE users (
  	phone varchar(255)
  );
 
+CREATE TABLE clothes (
+ owner varchar(255),
+ client varchar(255) DEFAULT '',
+ name varchar(255) PRIMARY KEY,
+ category varchar(255),
+ size varchar(255),
+ location varchar(255),
+ posted varchar(255) DEFAULT 'true',
+ date   varchar(255),
+ time   timestamp,
+ image  varchar(255)
+);
